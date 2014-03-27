9e8fa0084a76ce88e549ad15c147592856494f0d
Michael Yanagisawa <michael.yanagisawa@gmail.com>
2014-03-26 21:36:40 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Marchapp::Application.config.secret_key_base = 'ae81f4e2d7f75b1a9dee5a240732f15d8f7aa2a112804cc19c11ad7cf4595985aa4acb2df6d36842777491caf89abeb47acf18dcf60deab83f4a38ab1bdcf1b5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Marchapp::Application.config.session_store :cookie_store, key: '_marchapp_session'
